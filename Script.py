d7d8580d44022d2c610ac524503d7882a0e582f9
Srikanth Nayak <130298853+Srikanth5657@users.noreply.github.com>
2023-07-05 13:07:33 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,7 +1,7 @@
 class script(object):
     START_TXT = """👋 𝙷𝙴𝙻𝙾 {} 💖
     
-𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 {}
+𝙼𝚈 𝙽𝙰𝙼𝙴 𝙸𝚂 <a href='https://t.me/GmSearchBot'>GM SEARCH BOT</b>
 𝙸 𝙲𝙰𝙽 𝙿𝚁𝙾𝚅𝙸𝙳𝙴 𝙼𝙾𝚅𝙸𝙴𝚂, 𝙹𝚄𝚂𝚃 𝙰𝙳𝙳 𝙼𝙴 𝚃𝙾 𝚈𝙾𝚄𝚁 𝙶𝚁𝙾𝚄𝙿 𝙰𝙽𝙳 𝙼𝙰𝙺𝙴 𝙼𝙴 𝙰𝙳𝙼𝙸𝙽..."""
 
     HELP_TXT = """🙋🏻‍♂️   𝖧𝖾𝗅𝗅𝗈𝗈𝗈  {} 🤓
