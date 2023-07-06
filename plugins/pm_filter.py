072901cf0a9a10ef6e19731bf06623180b36b446
Srikanth Nayak <130298853+Srikanth5657@users.noreply.github.com>
2023-07-06 17:28:03 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -538,7 +538,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             file_id=file_id,
             caption=f_caption,
             protect_content=True if ident == 'checksubp' else False,
-            
+        ) 
     elif query.data == "pages":
         await query.answer()
 
