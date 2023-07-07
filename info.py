de1d9594205f32cc7b24251bf50e3d8455aa977d
Srikanth Nayak <130298853+Srikanth5657@users.noreply.github.com>
2023-07-07 22:11:41 +0530

    Update info.py

--- info.py
+++ info.py
@@ -48,6 +48,7 @@ support_chat_id = environ.get('SUPPORT_CHAT_ID')
 reqst_channel = environ.get('REQST_CHANNEL_ID')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
 SUPPORT_CHAT_ID = -1001379487479
+SHORTLINK = is_enabled((environ.get('SHORTLINK', "True")), True)
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
 
 # MongoDB information
@@ -72,6 +73,8 @@ IMDB_TEMPLATE = environ.get("IMDB_TEMPLATE", '🏷 𝖳𝗂𝗍𝗅𝖾: <a href
 LONG_IMDB_DESCRIPTION = is_enabled(environ.get("LONG_IMDB_DESCRIPTION", "False"), False)
 SPELL_CHECK_REPLY = is_enabled(environ.get("SPELL_CHECK_REPLY", "True"), True)
 MAX_LIST_ELM = environ.get("MAX_LIST_ELM", None)
+SHORTLINK_URL = environ.get("SHORTLINK_URL", "powerlinkz.in")
+SHORTLINK_API = environ.get("SHORTLINK_API", "67998a98a9fae359d6cb3cb57bc4114fe99563c5")
 INDEX_REQ_CHANNEL = int(environ.get('INDEX_REQ_CHANNEL', LOG_CHANNEL))
 FILE_STORE_CHANNEL = [int(ch) for ch in (environ.get('FILE_STORE_CHANNEL', '')).split()]
 MELCOW_NEW_USERS = is_enabled((environ.get('MELCOW_NEW_USERS', "True")), True)
