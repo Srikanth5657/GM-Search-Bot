7b52fbd6032b1639b6badead8bcaf94d60c9b562
Srikanth Nayak <130298853+Srikanth5657@users.noreply.github.com>
2023-07-16 00:22:20 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1406,8 +1406,7 @@ async def advantage_spell_chok(client, msg):
         await asyncio.sleep(30)
         await k.delete()
         return
-    movielist += [movie.get('title') for movie in movies]
-    movielist += [f"{movie.get('title')} {movie.get('year')}" for movie in movies]
+    movielist = []
     
     if not movies:
         reqst_gle = mv_rqst.replace(" ", "+")
