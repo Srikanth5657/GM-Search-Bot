aa7479bb9701e406102aeda271fb382a41372f11
Srikanth Nayak <130298853+Srikanth5657@users.noreply.github.com>
2023-07-16 20:04:27 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -488,3 +488,4 @@ async def send_all(bot, userid, files, ident):
             file_id=file.file_id,
             caption=f_caption,
             protect_content=True if ident == "filep" else False
+        )
