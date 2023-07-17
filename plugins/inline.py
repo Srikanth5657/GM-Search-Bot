52a00bc75dd9027174921e87e510de3dbfc7afca
Srikanth Nayak <130298853+Srikanth5657@users.noreply.github.com>
2023-07-17 15:15:21 +0530

    Update inline.py

--- plugins/inline.py
+++ plugins/inline.py
@@ -108,9 +108,7 @@ def get_reply_markup(query):
     buttons = [
         [
             InlineKeyboardButton('🔎 𝖲𝖾𝖺𝗋𝖼𝗁 𝖠𝗀𝖺𝗂𝗇', switch_inline_query_current_chat=query),
-            InlineKeyboardButton('⚡𝖴𝗉𝖽𝖺𝗍𝖾𝗌 ⚡', url="https://t.me/GMOriginals")
+            InlineKeyboardButton('⚡𝖴𝗉𝖽𝖺𝗍𝖾𝗌 ⚡', url="https://t.me/GeethaMovies")
         ]
         ]
     return InlineKeyboardMarkup(buttons)
-
-
