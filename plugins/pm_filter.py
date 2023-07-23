aba9c2578f287255c6c4b7e3e5e2f36cc8773a5d
Srikanth Nayak <130298853+Srikanth5657@users.noreply.github.com>
2023-07-23 11:24:10 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -1218,9 +1218,7 @@ async def auto_filter(client, msg, spoll=False):
             user_id = message.from_user.id
             files, offset, total_results = await get_search_results(message.chat.id ,search.lower(), offset=0, filter=True)
             if not files:
-                await client.send_message(chat_id=REQST_CHANNEL,f"-🦋 #REQUESTED_CONTENT 🦋-\n\n📝**Content Name** :`{search}`\n**Requested By**: {message.from_user.first_name}\n **USER ID**:{user_id}\n\n🗃️",
-                                                                                                       reply_markup=InlineKeyboardMarkup([
-                                                                                                                                        [InlineKeyboardButton(text=f"✅Upload Done", callback_data=f"close_data")]]))
+                await client.send_message(chat_id=REQST_CHANNEL,f"-🦋 #REQUESTED_CONTENT 🦋-\n\n📝**Content Name** :`{search}`\n**Requested By**: {message.from_user.first_name}\n **USER ID**:{user_id}\n\n🗃️")
                 await asyncio.sleep(20)
                 await l.delete()
                 if settings["spell_check"]:
